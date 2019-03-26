e397bab19f74126fd160b04804a2f21b7967f437
cgranthovey <christopherhovey@Christophers-MacBook-Pro.local>
2019-03-25 21:59:32 -0600

    First commit

---
+++ FruitVegi/VC/ListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListVC.swift
+//  FruitVegi
+//
+//  Created by Christopher Hovey on 3/25/19.
+//  Copyright © 2019 Chris Hovey. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ FruitVegi/View/NavBar.swift
@@ -0,0 +1,29 @@
+//
+//  NavBar.swift
+//  FruitVegi
+//
+//  Created by Christopher Hovey on 3/25/19.
+//  Copyright © 2019 Chris Hovey. All rights reserved.
+//
+
+import UIKit
+
+class NavBar: UINavigationController {
+    
+    override func awakeFromNib() {
+//        navigationBar.barStyle = .black
+        navigationBar.barTintColor = UIColor.brown
+//        navigationBar.isTranslucent = false
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
